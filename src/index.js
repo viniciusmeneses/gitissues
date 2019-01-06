b4879cb3df073d78999f6f273dde2ad050a55304
viniciusmeneses <vini23hot@gmail.com>
2019-01-06 12:04:36 -0200

    Started app project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import createNavigator from './routes';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Routes />
+    );
+  }
+}
